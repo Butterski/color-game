9a697c8e15aaf016215b6ac93c1a03cefe81e0dd
milosz.kucharski <miloszkucharski@gmail.com>
2022-11-04 23:42:48 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [goodColor, setGoodColor] = useState<string>("");
+  const [colors, setColors] = useState<any>();
+  const [score, setScore] = useState<number>(0);
+
+  function rgbToHex(r: number, g: number, b: number) {
+    return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+  }
+
+  function generateRandomColorHex() {
+    var arr = [];
+    for (var i = 0; i < 3; i++) {
+      arr.push(Math.floor(Math.random() * 256));
+    }
+    return (rgbToHex(arr[0], arr[1], arr[2])).toUpperCase();
+  }
+
+  function setupGame() {
+    let colours = [];
+    for (var i = 0; i < 4; i++) {
+      colours.push(generateRandomColorHex());
+    }
+    setGoodColor(colours[Math.floor(Math.random() * 4)]);
+    setColors(colours);
+  }
+
+  function colorPick(color: string) {
+    if (color === goodColor) {
+      setScore(score + 1);
+      setupGame();
+    } else {
+      setScore(0);
+      setupGame();
+    }
+  }
+
+  useEffect(() => {
+    setupGame();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="score-board">Score: {score}</div>
+      <div className="color-square" style={{ background: goodColor }}>
+        {goodColor}
+      </div>
+      <div>
+        {colors?.map((value: any, index: number) => {
+          return (
+            <button key={index} onClick={() => colorPick(value)}>
+              {value}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ todo.md
@@ -0,0 +1,7 @@
+# TODO
+- generujesz losowy kolor
+- generujesz dodatkowe 3 kolory
+- pokazujesz główny kolor
+- pokazujesz 4 pola z wyborem z wszystkimi kolorwami
+- user wybiera jedną opcję
+- jeśli kolor zgadza się hex kodowi to user dostaje punkt jeśli nie restart
