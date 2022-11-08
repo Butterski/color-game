e7c9b6ddb588d9e9c4bfbfd94fb73632293568b3
butterski <miloszkucharski@gmail.com>
2022-11-08 16:21:42 +0100

    added posibility to add more games

---
+++ src/games/Maker.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './maker.css';
+
+const Maker = () => {
+    return(
+        <div className="Maker">
+            yo
+        </div>
+    )
+}
+
+export default Maker;
